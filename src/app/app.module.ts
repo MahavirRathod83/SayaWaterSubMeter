45bb69c881fe9de5a21488cfdd3367faeb5832d8
Princemaxlink <prince.sen@sayalife.in>
2023-12-22 12:10:34 +0530

    Header Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WaterSubmeterComponent } from './components/water-submeter/water-submeter.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WaterSubmeterComponent
+    WaterSubmeterComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
