606dcde589172fe2fa5116fad66d4278c8384143
Princemaxlink <prince.sen@sayalife.in>
2023-12-27 14:10:35 +0530

    about updated

--- src/app/components/water-submeter/water-submeter.component.ts
+++ src/app/components/water-submeter/water-submeter.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-water-submeter',
   templateUrl: './water-submeter.component.html',
-  styleUrls: ['./water-submeter.component.css']
+  styleUrls: ['./water-submeter.component.css'],
 })
 export class WaterSubmeterComponent {
-
+  constructor(private render: Renderer2, private el: ElementRef) {}
 }
