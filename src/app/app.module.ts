6f6b40f50569396a2f24b5ff4368ade20c36b068
Mahavir <mahavirsinh.rathod@sayalife.in>
2023-12-27 12:03:56 +0530

    header toggle button and accordion issue solved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { FooterComponent } from './layouts/footer/footer.component';
 import { WaterSubmeterComponent } from './components/water-submeter/water-submeter.component';
 import { CountUpModule } from 'ngx-countup';
 import { AboutComponent } from './components/about/about.component';
-
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -17,13 +17,8 @@ import { AboutComponent } from './components/about/about.component';
     FooterComponent,
     WaterSubmeterComponent,
     AboutComponent,
-   
   ],
-  imports: [
-    BrowserModule,
-      AppRoutingModule,
-      CountUpModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, CountUpModule, NgbDropdownModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { routes } from 'src/app/shared/routes/routes';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-
+  public routes = routes;
 }
