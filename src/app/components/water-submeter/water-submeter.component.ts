dc2d8474f63116111c29de800f87c28866612439
Princemaxlink <prince.sen@sayalife.in>
2024-02-05 15:37:33 +0530

    website changes done

--- src/app/components/water-submeter/water-submeter.component.ts
+++ src/app/components/water-submeter/water-submeter.component.ts
@@ -1,8 +1,55 @@
 import { Component } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-water-submeter',
   templateUrl: './water-submeter.component.html',
   styleUrls: ['./water-submeter.component.css'],
 })
-export class WaterSubmeterComponent {}
+export class WaterSubmeterComponent {
+  constructor(meta: Meta, title: Title) {
+    title.setTitle('water submeter');
+    meta.addTags([
+      {
+        name: 'keywords',
+        content: 'Casubmetering Solutions',
+      },
+      {
+        name: 'keywords',
+        content: 'water submetering',
+      },
+      {
+        name: 'keywords',
+        content: 'Submeter Solutions',
+      },
+      {
+        name: 'keywords',
+        content: 'Utility Billing',
+      },
+      {
+        name: 'keywords',
+        content: 'Large Installations',
+      },
+      {
+        name: 'keywords',
+        content: 'Property Management Solutions',
+      },
+      {
+        name: 'keywords',
+        content: 'Revolutionary Leak Prevention',
+      },
+      {
+        name: 'keywords',
+        content: 'Water Leak Prevention',
+      },
+      {
+        name: 'keywords',
+        content: 'Billing Platform',
+      },
+      {
+        name: 'description',
+        content: 'Discover the power of saya',
+      },
+    ]);
+  }
+}
